fb4d12ddba2e2aa499851d7aa9796c312f7305e4
Casey Tin <ctin@umich.edu>
2020-03-14 12:11:08 -0400

    progress push

---
+++ pages/form.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+    First name: <?php echo $_POST["firstname"]; ?><br>
+    Last name: <?php echo $_POST["lastname"]; ?><br>
+    Email address: <?php echo $_POST["email"]; ?>
+    
+
+</body>
+</html>
